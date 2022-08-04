4a18cfa368792934400167158874f311bf7ab046
Brien Thomas <thomasbrien94@gmail.com>
2022-08-04 14:32:03 -0700

    SQL table for coffee created

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,10 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS donuts;
--- DROP TABLE IF EXISTS sports;
--- DROP TABLE IF EXISTS books;
--- DROP TABLE IF EXISTS movies;
--- DROP TABLE IF EXISTS dogs;
+DROP TABLE IF EXISTS coffee;
 
 CREATE TABLE donuts (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -22,4 +19,23 @@ VALUES
 ('Portland Fog', 'Earl Grey Glaze, Fresh Whip, Vanilla Bean'),
 ('Blackberry, Mint, and Cucumber', 'Blackberry Mint, Cucumber Glaze, Mint Leaf'),
 ('Lemon Poppy Twist', 'Lemon Glaze, Lemon Zest, Poppy Seeds'),
-('Cheeseburger', 'Burger Patty, Cheddar, Special Sauce, House Made Dill Pickle')
+('Cheeseburger', 'Burger Patty, Cheddar, Special Sauce, House Made Dill Pickle');
+
+CREATE TABLE coffee (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR,
+    grind_type VARCHAR,
+    time VARCHAR
+);
+
+INSERT INTO coffee (
+    name,
+    grind_type,
+    time
+)
+VALUES
+('Pour Over - Coffee Cone', 'Med. Fine to Coarse', '2-3 Minutes'),
+('Pour Over - Chemex', 'Medium to Coarse', '5-10 Minutes'),
+('French Press', 'Coarse', '5 Minutes'),
+('AeroPress', 'Fine to Medium', '2-3 Minutes'),
+('Moka Pot', 'Fine to Med. Coarse', '5 Minutes');
