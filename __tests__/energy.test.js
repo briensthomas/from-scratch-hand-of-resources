667f50baa9600fbe89d39d89fa33ba910df08d31
Brien Thomas <thomasbrien94@gmail.com>
2022-08-04 17:20:39 -0700

    Testing for PUT route created, expects to update the flavor and rating of a specific energy drink, and see a status of 200

--- __tests__/energy.test.js
+++ __tests__/energy.test.js
@@ -44,7 +44,15 @@ describe('backend-express-template routes', () => {
     });
   });
 
-  
+  it('#PUT /energy/:id should update a row on the table by id', async () => {
+    const res = await request(app).put('/energy/1').send({
+      flavor: 'Green',
+      rating: 10
+    });
+    expect(res.status).toBe(200);
+    expect(res.body.flavor).toBe('Green');
+    expect(res.body.rating).toBe(10);
+  });
   afterAll(() => {
     pool.end();
   });
