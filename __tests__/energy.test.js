90abc24d37ded11fea533e4637d6a23d6af34c11
Brien Thomas <thomasbrien94@gmail.com>
2022-08-04 17:06:03 -0700

    test written for GET by ID route, expects a status 200 and the 4 keys with their value types

--- __tests__/energy.test.js
+++ __tests__/energy.test.js
@@ -9,12 +9,24 @@ describe('backend-express-template routes', () => {
   });
   it('#GET /energy should display a list of all energy drink objects', async () => {
     const res = await request(app).get('/energy');
+    expect(res.status).toBe(200);
     expect(res.body.length).toBe(5);
     expect(res.body[3]).toEqual({
       id: expect.any(String),
       name: 'Monster Energy Drink',
       flavor:  'Gronkster',
       rating: 10
+    });
+  });
+
+  it('#GET /energy/:id should display information for one energy drink', async () => {
+    const res = await request(app).get('/energy/5');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      flavor: expect.any(String),
+      rating: expect.any(Number)  
     });
   });
   afterAll(() => {
